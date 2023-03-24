ac5470bdfb632a1ef365cee1d2668dfc0b183690
Buntu William <williambuntu1@gmail.com>
2023-03-24 16:50:50 +0200

    2rd commit

---
+++ pass.py
@@ -0,0 +1,29 @@
+class Flight():
+    def __init__(self,capacity):
+        self.capacity = capacity
+        self.passengers = []
+ 
+def add_passenger(self,name):
+    if not open_saets():
+        return False
+    self.passengers.append(name) 
+    return True
+
+def open_saets(self):
+    return self.capacity - len(self.passengers)
+
+flight = Flight(4)
+
+people = ['Buntu','Alain','Mashami','Mugisha','Rutinywa']
+   
+
+for person in people:
+    success = flight.add_passenger(self,person)
+    if success:
+        print(f'Added {person} to the flight successfully')
+    else:
+        print(f'no seat available for {person}')
+
+
+
+        
